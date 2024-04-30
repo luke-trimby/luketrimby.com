aed20b31b3a1571064da614a7640475c42163f6a
Luke Trimby <luke@lukas-tech.com>
2024-04-30 15:07:19 +0100

    Updates

--- pages/api/cv.ts
+++ pages/api/cv.ts
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const pipeline = promisify(stream.pipeline);
-const url = 'https://drive.google.com/file/d/1OZjVlWGc3Tn0B3djMBWSjUyCFmDIpo3b/view?usp=drive_link';
+const url = 'https://drive.google.com/file/d/1mMeHLsECh9mKuwz0P8opzFJqXcMEnmgA/view?usp=sharing';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await fetch(url); // replace this with your API call & options
